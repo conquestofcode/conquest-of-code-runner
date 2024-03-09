150bff865e4edd6082987d31ce1226a7ad26e526
MatthewWHuang <64226660+MatthewWHuang@users.noreply.github.com>
2024-03-09 18:34:16 -0500

    v1-1.0.0-3.19.2 windows build

--- players/exampleplayer/exampleplayer.py
+++ players/exampleplayer/exampleplayer.py
@@ -1,12 +1,14 @@
 import player
 from robot import Robot
-import random
+from random import Random
 from common import *
+
 
 class Player(player.Player):
     """Example player that moves, collects, and attacks randomly."""
 
-    def __init__(self, rc: Robot, rng: random.Random):
+    def __init__(self, rc: Robot, rng: Random):
+        global message
         self.rc = rc
         self.turnCount = 0
         self.rng = rng
@@ -15,9 +17,9 @@ class Player(player.Player):
     def update(self):
         rc = self.rc
         if rc.getType() == RobotType.CASTLE:
-            self.runCastle(rc) 
+            self.runCastle(rc)
         elif rc.getType() == RobotType.KNIGHT:
-            self.runKnight(rc) 
+            self.runKnight(rc)
         elif rc.getType() == RobotType.VILLAGE:
             self.runVillage(rc)
         elif rc.getType() == RobotType.VILLAGER:
@@ -27,45 +29,44 @@ class Player(player.Player):
         elif rc.getType() == RobotType.CATAPULT:
             self.runCatapult(rc)
         elif rc.getType() == RobotType.TREBUCHET:
-            self.runTrebuchet(rc) 
-    
-    def runCastle(self, rc:Robot):
+            self.runTrebuchet(rc)
+
+    def runCastle(self, rc: Robot):
         target = rc.getLocation().add(self.rng.choice(self.DIRECTIONS))
         if rc.canTrain(RobotType.KNIGHT, target):
             rc.train(RobotType.KNIGHT, target)
-    
-    def runSiegeFoundry(self, rc:Robot):
+
+    def runSiegeFoundry(self, rc: Robot):
         target = rc.getLocation().add(self.rng.choice(self.DIRECTIONS))
         if rc.canTrain(RobotType.TREBUCHET, target):
             rc.train(RobotType.TREBUCHET, target)
         elif rc.canTrain(RobotType.CATAPULT, target):
             rc.train(RobotType.CATAPULT, target)
 
-    def runKnight(self, rc:Robot):
-        robots = list(filter(lambda robot: robot.getTeam() != rc.getTeam(), rc.senseNearbyRobots(rc.getType().ACTION_RANGE)))
+    def runKnight(self, rc: Robot):
+        robots = list(
+            filter(
+                lambda robot: robot.getTeam() != rc.getTeam(),
+                rc.senseNearbyRobots(rc.getType().ACTION_RANGE),
+            )
+        )
         if len(robots) > 0:
-            robot:Robot = robots[0]
+            robot: Robot = robots[0]
             if rc.canAttack(robot.getLocation()):
                 rc.attack(robot.getLocation())
         target = rc.getLocation().add(self.rng.choice(self.DIRECTIONS))
         if rc.canMove(target=target):
             rc.move(target=target)
-    
-    def runCatapult(self, rc:Robot):
-        robots = list(filter(lambda robot: robot.getTeam() != rc.getTeam(), rc.senseNearbyRobots(rc.getType().ACTION_RANGE)))
+
+    def runCatapult(self, rc: Robot):
+        robots = list(
+            filter(
+                lambda robot: robot.getTeam() != rc.getTeam(),
+                rc.senseNearbyRobots(rc.getType().ACTION_RANGE),
+            )
+        )
         if len(robots) > 0:
-            robot:Robot = robots[0]
-            if rc.canAttack(robot.getLocation()):
-                rc.attack(robot.getLocation())
-        else:
-            target = rc.getLocation().add(self.rng.choice(self.DIRECTIONS))
-            if rc.canMove(target=target):
-                rc.move(target=target)
-    
-    def runTrebuchet(self, rc:Robot):
-        robots = list(filter(lambda robot: robot.getTeam() != rc.getTeam(), rc.senseNearbyRobots(rc.getType().ACTION_RANGE)))
-        if len(robots) > 0:
-            robot:Robot = robots[0]
+            robot: Robot = robots[0]
             if rc.canAttack(robot.getLocation()):
                 rc.attack(robot.getLocation())
         else:
@@ -73,12 +74,28 @@ class Player(player.Player):
             if rc.canMove(target=target):
                 rc.move(target=target)
 
-    def runVillage(self, rc:Robot):
+    def runTrebuchet(self, rc: Robot):
+        robots = list(
+            filter(
+                lambda robot: robot.getTeam() != rc.getTeam(),
+                rc.senseNearbyRobots(rc.getType().ACTION_RANGE),
+            )
+        )
+        if len(robots) > 0:
+            robot: Robot = robots[0]
+            if rc.canAttack(robot.getLocation()):
+                rc.attack(robot.getLocation())
+        else:
+            target = rc.getLocation().add(self.rng.choice(self.DIRECTIONS))
+            if rc.canMove(target=target):
+                rc.move(target=target)
+
+    def runVillage(self, rc: Robot):
         target = rc.getLocation().add(self.rng.choice(self.DIRECTIONS))
         if rc.canTrain(RobotType.VILLAGER, target):
             rc.train(RobotType.VILLAGER, target)
 
-    def runVillager(self, rc:Robot):
+    def runVillager(self, rc: Robot):
         target = rc.getLocation().add(self.rng.choice(self.DIRECTIONS))
         if rc.canMove(target=target):
             rc.move(target=target)
